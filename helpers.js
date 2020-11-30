e2f6f1c7ea6c954d6930e08a710f9ac37191f8a8
Benjamin <=>
2020-11-30 14:45:51 +0100

    new commit

--- helpers.js
+++ helpers.js
@@ -1,15 +1,17 @@
-
 export function randomArray(len, n) {
-
     // Skapa en ny tom array av längd len
     const a = new Array(len)
-
     // Fyll varje plats i arrayen med ett random heltal
     for (let i = 0; i < a.length; i++) {
         a[i] = randomInt(n)
     }
-
     return a
+}
+
+export function swap(arr, number1, number2) {
+    const temp = arr[number1]
+    arr[number1] = arr[number2]
+    arr[number2] = temp
 }
 
 function randomInt(n) {
--- sorting.js
+++ sorting.js
@@ -1,3 +1,14 @@
 import { randomArray } from './helpers.js'
+import { swap } from './helpers.js'
 
 const a = randomArray(100, 10)
+
+const bubbleSort = (array) => {
+    for (let j = 0; j < array.length - 1; j++) {
+        for (let i = 0; i < array.length - 1 - j; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(array, i, i + 1)
+            }
+        }
+    }
+}
